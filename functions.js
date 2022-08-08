5598bb61d111e87d139c6e3c1f688b70cef2400c
CKWheatley <88515213+CKWheatley@users.noreply.github.com>
2022-08-08 23:34:07 +0100

    1.0
    Additions:
    
    Added the generic note tool for the system to make it easy/fool proof to make notes then copy them across.

--- functions.js
+++ functions.js
@@ -1,1 +1,69 @@
 // JavaScript Document
+
+var type = $("#contact-type")
+var title = $("#title")
+var contact_name = $("#contact-name")
+var details = $("#contact-details")
+var summary = $("#summary")
+var action = $("#action-required")
+var follow = $("#follow-up-required")
+
+var label = $("label")
+var input = $("input")
+var select = $("select")
+var textarea = $("textarea")
+
+var template = $("#gen-text")
+
+select.change(() => {
+    if(type.find(select).val() == "Call"){
+        title.find(label).html("Title (Call Topic):")
+        contact_name.find(label).html("Caller's Full Name:")
+        details.find(label).html("Contact Number:")
+        details.removeClass("hidden")
+    }else{
+        title.find(label).html(" Title (Email Topic):")
+        contact_name.find(label).html("Contacts Full Name:")
+        details.addClass("hidden")
+        details.find(label).html("")
+        details.find(input).val("")
+    }
+})
+
+input.change(() => {
+    if(type.find(select).val() == "Call"){
+        template.html(`
+        ${contact_name.find(label).html()} ${contact_name.find(input).val()}
+        <br>
+        ${details.find(label).html()} ${details.find(input).val()}
+        <br>
+        ${summary.find(label).html()} ${summary.find(textarea).val()}
+        <br>
+        ${action.find(label).html()} ${action.find(input).val()}
+        <br>
+        ${follow.find(label).html()} ${follow.find(input).val()}
+        `)
+    }else{
+        template.html(`
+        ${contact_name.find(label).html()} ${contact_name.find(input).val()}
+        <br>
+        ${summary.find(label).html()} ${summary.find(textarea).val()}
+        <br>
+        ${action.find(label).html()} ${action.find(input).val()}
+        <br>
+        ${follow.find(label).html()} ${follow.find(input).val()}
+        `)
+    }
+    
+})
+
+/*
+function copy(){
+    template.select()
+    document.execCommand("copy");
+}
+
+$("#copy").click(() => {
+    copy()
+})
+*/
