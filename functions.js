8c69a814f27c74f1398484e5c118d707a3d6f421
CKWheatley <88515213+CKWheatley@users.noreply.github.com>
2022-08-11 23:06:04 +0100

    1.2
    Bug Fixes
    
    Additions
    Added 2 other buttons and text areas
    Slightly better small screen compatibility for when you need that screen space
    
    Changes
    Colour changes & styling differences

--- functions.js
+++ functions.js
@@ -13,7 +13,9 @@ var input = $("input")
 var select = $("select")
 var textarea = $("textarea")
 
-var template = $("#gen-text")
+var desc_template = $("#desc-gen-text")
+var title_template = $("#title-gen-text")
+var action_template = $("#action-gen-text")
 
 select.change(() => {
     if(type.find(select).val() == "Call"){
@@ -32,24 +34,27 @@ select.change(() => {
 
 function update(){
     if(type.find(select).val() == "Call"){
-        template.html(`${contact_name.find(label).html()} ${contact_name.find(input).val()}
+        title_template.html(`${title.find(input).val()}`)
+
+        desc_template.html(`${contact_name.find(label).html()} ${contact_name.find(input).val()}
 ${details.find(label).html()} ${details.find(input).val()}
-${summary.find(label).html()} ${summary.find(textarea).val()}
-${action.find(label).html()} ${action.find(input).val()}
-${follow.find(label).html()} ${follow.find(input).val()}
-`)
+${summary.find(label).html()} ${summary.find(textarea).val()}`)
+
+        action_template.html(`${action.find(label).html()} ${action.find(input).val()}
+${follow.find(label).html()} ${follow.find(input).val()}`)
+
 // if action and follow is empty then dont populate.
 // add if form condition
 // complete crm task notes thing
     }
     else{
-        template.html(`${contact_name.find(label).html()} ${contact_name.find(input).val()}
+        title_template.html(`${title.find(input).val()}`)
+        desc_template.html(`${contact_name.find(label).html()} ${contact_name.find(input).val()}
 ${summary.find(label).html()} ${summary.find(textarea).val()}
-${action.find(label).html()} ${action.find(input).val()}
-${follow.find(label).html()} ${follow.find(input).val()}
 `)
+        action_template.html(`${action.find(label).html()} ${action.find(input).val()}
+${follow.find(label).html()} ${follow.find(input).val()}`)
     }
-    
 }
 input.change(() => {
     update()
@@ -65,11 +70,18 @@ function copy(value){
     document.execCommand("copy");
 }
 
-$("#copy").click(() => {
-    copy(template)
+$("#desc-copy").click(() => {
+    copy(desc_template)
 })
+$("#title-copy").click(() => {
+    copy(title_template)
+})
+$("#action-copy").click(() => {
+    copy(action_template)
+})
+
 /*
-$(template).each(function () {
+$(desc_template).each(function () {
     this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
   }).on("input", function () {
     this.style.height = "auto";
@@ -78,3 +90,5 @@ $(template).each(function () {
 
 https://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize
 */
+
+// add char counter function to check whether it will go over the message limit. Figure it out by spamming letters in msg then store as var.val().length
